80e12be37def5d6902032a4bb5239593c16c739d
Henrique <henriquelimadasilva.90@gmail.comgit config --global user.email henriquelimadasilva.90@gmail.com>
2019-05-22 21:28:49 -0300

    Changed the HTML for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import  views
 
 urlpatterns = [
     path('', views.post_list, name ='post_list'),
-    path('contato', views.post_list_2, name ='post_list'),
+    path('contato', views.post_list_2, name ='post_list_2'),
 ]
