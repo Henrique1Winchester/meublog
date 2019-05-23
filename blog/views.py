d36cfe1b866644c2238ebf96705b51297d703668
Henrique <henriquelimadasilva.90@gmail.comgit config --global user.email henriquelimadasilva.90@gmail.com>
2019-05-22 21:27:16 -0300

    Criando a pagina inicial

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
-def post_list2(request):
+def post_list_2(request):
     return render(request, 'blog/post_list.html', {})
